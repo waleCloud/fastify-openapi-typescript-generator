d66a13bf830119ae53a0f24312890a53bcd28486
Stefano Righini <stefano.righini@quinck.io>
2022-08-15 10:50:45 +0200

    add work in progress test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,29 +1,8 @@
-import { OpenApiVersionVerifier } from './../src/openapi-version-verifier/openapi-version-verifier'
-import { OpenapiParser } from './../src/openapi-parser/openapi-parser'
 import 'mocha'
-import { HandlersGenerator, RoutesSchemasGenerator } from '../src/index'
-import { OpenapiReader } from '../src/openapi-reader/openapi-reader'
-import fs from 'fs'
+import { expect } from 'chai'
 
 describe('Dummy', () => {
-    it('hello world', () => {
-        // const openapiPath = `${__dirname}/../local/openapi.yml`
-        // const reader = new OpenapiReader()
-        // const parser = new OpenapiParser()
-        // const versionVerifier = new OpenApiVersionVerifier()
-        // const routesSchemasGenerator = new RoutesSchemasGenerator(
-        //     reader,
-        //     parser,
-        //     versionVerifier,
-        //     openapiPath,
-        // )
-        // const result = routesSchemasGenerator.generateRoutesSchemas()
-        // const handlersGenerator = new HandlersGenerator(
-        //     reader,
-        //     parser,
-        //     openapiPath,
-        // )
-        // const handlers = handlersGenerator.generateHandlers()
-        console.log('hello world')
+    it('Work in progress', () => {
+        expect(true).to.be.true
     })
 })
