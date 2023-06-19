59b8c6b846a3dcf9a6da692c64353d822623952c
Stefano Righini <stefano.righini@quinck.io>
2023-06-19 10:58:00 +0200

    feat(route-configs): ✨ use import.meta.url instead of __dirname since the package is a module

--- src/openapi-types/openapi-types.ts
+++ src/openapi-types/openapi-types.ts
@@ -1,9 +1,9 @@
-import openapiTS from '@quinck/openapi-typescript'
+import openapiTS from 'openapi-typescript'
 
 export const generateOpenapiTypes = (
     openapiFilePath: string,
 ): Promise<string> => {
-    const localPath = new URL(openapiFilePath, __dirname)
+    const localPath = new URL(openapiFilePath, import.meta.url)
 
     return openapiTS(localPath)
 }
