707b54fd7857d460b428725b1ab52fdf54e7acc2
Stefano Righini <stefano.righini@quinck.io>
2023-06-19 10:44:20 +0200

    feat(route-configs): ✨ add command to create route configs for fastify from an openapi specification

--- src/openapi-types/openapi-types.ts
+++ src/openapi-types/openapi-types.ts
@@ -3,7 +3,7 @@ import openapiTS from '@quinck/openapi-typescript'
 export const generateOpenapiTypes = (
     openapiFilePath: string,
 ): Promise<string> => {
-    const localPath = new URL(openapiFilePath, import.meta.url)
+    const localPath = new URL(openapiFilePath, __dirname)
 
     return openapiTS(localPath)
 }
