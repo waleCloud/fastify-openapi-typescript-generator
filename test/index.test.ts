9aadfa62c2de668c9d8dfe9a33563d29e46aa7f1
waleCloud <walexrvp@gmail.com>
2024-07-26 23:45:05 +0100

    update sample test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,8 +1,29 @@
+import { OpenApiVersionVerifier } from './../src/openapi-version-verifier/openapi-version-verifier'
+import { OpenapiParser } from './../src/openapi-parser/openapi-parser'
 import 'mocha'
-import { helloWorld } from '../src/index'
+import { HandlersGenerator, RoutesSchemasGenerator } from '../src/index'
+import { OpenapiReader } from '../src/openapi-reader/openapi-reader'
+import fs from 'fs'
 
 describe('Dummy', () => {
     it('hello world', () => {
-        helloWorld()
+        // const openapiPath = `${__dirname}/../local/openapi.yml`
+        // const reader = new OpenapiReader()
+        // const parser = new OpenapiParser()
+        // const versionVerifier = new OpenApiVersionVerifier()
+        // const routesSchemasGenerator = new RoutesSchemasGenerator(
+        //     reader,
+        //     parser,
+        //     versionVerifier,
+        //     openapiPath,
+        // )
+        // const result = routesSchemasGenerator.generateRoutesSchemas()
+        // const handlersGenerator = new HandlersGenerator(
+        //     reader,
+        //     parser,
+        //     openapiPath,
+        // )
+        // const handlers = handlersGenerator.generateHandlers()
+        console.log('hello world')
     })
 })
