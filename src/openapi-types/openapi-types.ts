5df93304dd8304f7ffbd344f04718d30075ce64c
waleCloud <walexrvp@gmail.com>
2024-07-26 23:45:07 +0100

    feat: ✨ allow to use externally generated type imported from a library o a file with the option externalTypesImportFrom

--- src/openapi-types/openapi-types.ts
+++ src/openapi-types/openapi-types.ts
@@ -1,9 +1,34 @@
-import openapiTS from 'openapi-typescript'
+import openapiTS, { OpenAPITSOptions } from 'openapi-typescript'
+
+export type OpenapiTypesOptions = {
+    externalTypesImportFrom?: string
+}
+
+const schemaStartPath = '#/components/schemas/'
 
 export const generateOpenapiTypes = (
     openapiFilePath: string,
+    options?: OpenapiTypesOptions,
 ): Promise<string> => {
     const localPath = new URL(openapiFilePath, import.meta.url)
 
-    return openapiTS(localPath)
+    const { externalTypesImportFrom } = options ?? {}
+
+    const openAPITSOptions: OpenAPITSOptions = {}
+
+    if (externalTypesImportFrom) {
+        const externalTypesImport = 'openapiTypes'
+
+        openAPITSOptions.transform = (_, options) => {
+            if (options.path.startsWith(schemaStartPath)) {
+                const typeName = options.path.replace(schemaStartPath, '')
+
+                return `${externalTypesImport}.${typeName}`
+            }
+            return undefined
+        }
+        openAPITSOptions.inject = `import * as ${externalTypesImport} from "${externalTypesImportFrom}";`
+    }
+
+    return openapiTS(localPath, openAPITSOptions)
 }
