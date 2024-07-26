a59473a195b656b55e90126e00b75a771a7105ef
waleCloud <walexrvp@gmail.com>
2024-07-26 23:45:06 +0100

    feat(schemas-generation): ✨ define components to convert openapi schemas into typescript models

---
+++ src/commands/generate-schemas/generate-schemas-openapi-typescript-codegen.ts
@@ -0,0 +1,23 @@
+import { generate } from 'openapi-typescript-codegen'
+import {
+    SchemasGenerator,
+    SchemasGeneratorFactory,
+} from './generate-schemas.models'
+
+export const openapiTypescriptCodegen: SchemasGeneratorFactory = ({
+    openapi,
+    outputDirectory,
+}) => {
+    const generateSchemas: SchemasGenerator['generateSchemas'] = () =>
+        generate({
+            input: openapi,
+            output: outputDirectory,
+            exportCore: false,
+            exportServices: false,
+            exportSchemas: false,
+        })
+
+    return {
+        generateSchemas,
+    }
+}
